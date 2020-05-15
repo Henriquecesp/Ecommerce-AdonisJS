9c0a777609fa91d4af5829323837555454a96847
henriquecesp <henriquecesps4@gmail.com>
2020-05-15 00:41:38 -0300

    adonis-acl e migrations

--- app/Models/User.js
+++ app/Models/User.js
@@ -7,18 +7,28 @@ const Model = use('Model')
 const Hash = use('Hash')
 
 class User extends Model {
-  static boot () {
+  static boot() {
     super.boot()
 
     /**
      * A hook to hash the user password before saving
      * it to the database.
      */
-    this.addHook('beforeSave', async (userInstance) => {
+    this.addHook('beforeSave', async userInstance => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
       }
     })
+  }
+
+  /**
+   *  Adonis Acl Traits
+   */
+  static get traits() {
+    return [
+      '@provider:Adonis/Acl/HasRole',
+      '@provider:Adonis/Acl/HasPermission',
+    ]
   }
 
   /**
@@ -31,7 +41,7 @@ class User extends Model {
    *
    * @return {Object}
    */
-  tokens () {
+  tokens() {
     return this.hasMany('App/Models/Token')
   }
 }

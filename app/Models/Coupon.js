7264b90cbfc21b8b7871065f0d5c6f48f05d4d27
henriquecesp <henriquecesps4@gmail.com>
2020-05-18 00:12:27 -0300

    Coupon model and relation

---
+++ app/Models/Coupon.js
@@ -0,0 +1,37 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Coupon extends Model {
+  /**
+   *  Adonis only recognize timestamp as date
+   *  so this get says wich fields is date type
+   */
+  static get dates() {
+    return ['created_at', 'update_at', 'valid_from', 'valid_until']
+  }
+
+  /**
+   *  Relation with user
+   */
+  users() {
+    return this.belongsToMany('App/Models/User')
+  }
+
+  /**
+   *  Relation with products
+   */
+  products() {
+    return this.belongsToMany('App/Models/Products')
+  }
+
+  /**
+   *  Relation with orders
+   */
+  orders() {
+    return this.belongsToMany('App/Models/Order')
+  }
+}
+
+module.exports = Coupon

8e0a29194a538fee96eeb040199d39eaf96711d9
henrique <henriquecesps4@gmail.com>
2020-08-11 09:59:51 -0300

    client routes and controllers

--- start/routes/client.js
+++ start/routes/client.js
@@ -5,10 +5,18 @@ const Route = use('Route')
 
 Route.group(() => {
   /**
-   * Resource Routes
+   * Products Routes
    */
-  Route.resource('products', 'ProductController').apiOnly()
-  Route.resource('orders', 'OrderController').apiOnly()
+  Route.get('products', 'ProductController.index')
+  Route.get('products/:id', 'ProductController.show')
+
+  /**
+   * Orders Routes
+   */
+  Route.get('orders', 'OrderController.index')
+  Route.get('orders/:id', 'OrderController.show')
+  Route.post('orders', 'OrderController.store')
+  Route.put('orders/:id', 'OrderController.put')
 })
   .prefix('v1')
   .namespace('Client')
